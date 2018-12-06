c51b4412b36b6fdf87d15eed0df4b00842d330b8
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-12-06 12:32:31 -0800

    forgot about branch

---
+++ README.md
@@ -0,0 +1,4 @@
+# lab08 java-quotes
+This app is made to retrieve data from a json and show random popular book quotes.
+#creators
+Hiwot Nega & Kevin Rosales
---
+++ src/main/java/Quote.java
@@ -0,0 +1,21 @@
+public class Quote {
+    public String []tags;
+    public String author;
+    public String likes;
+    public String text;
+
+    public String[] getTags(){
+        return tags;
+    }
+
+    public String getAuthors(){
+        return author;
+    }
+    public String getLikes(){
+        return likes;
+    }
+    public String getText(){
+        return text;
+    }
+
+}
